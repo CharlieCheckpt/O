bd7bc835d7c2a82cb7d535e0fec87cb7524ab5a1
CharlieCheckpt <charlie.95@hotmail.fr>
2018-10-01 23:06:16 +0200

    clean

--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 import time
 import numpy as np
+import pandas as pd
 import os
+import pickle
 DATA_PATH = '../data'
 
 
@@ -25,3 +27,25 @@ def load_data(type_data: str):
     print(f"data loaded in {round(end-start, 3)} seconds")
 
     return X, y
+
+
+def convert_csv_2_hdf(filename:str):
+    """Read csv file line by line (too big to fit in memory) and write it to pickle file.
+    """
+    new_filename = filename.replace("csv", "pkl")
+
+    i = 0
+    with open(filename, "rb") as csvfile:
+        for line in csvfile:
+            print(f"line {i}")
+            # save
+            if i == 0:  # 1st time we create the file
+                with open(new_filename, "wb") as pklfile:
+                    pickle.dump(line, pklfile)
+            else:  # we just append to the file
+                with open(new_filename, 'ab') as pklfile:
+                    pickle.dump(line, pklfile)
+            
+            i = i + 1
+
+    print(f"data saved in {new_filename}")
