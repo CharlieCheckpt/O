3e2738fce8ba4dfb2208c3b47ec746f739b8252b
CharlieCheckpt <charlie.95@hotmail.fr>
2018-10-02 14:24:12 +0200

    run models own script

--- Catboost/run_cb.py
+++ Catboost/run_cb.py
@@ -5,30 +5,32 @@ import yaml
 import sys
 sys.path.insert(0, "../")
 from utils import load_data
-from xgb_model import Xgboost
+from cb_model import Catboost
 
 
 def main():
-
+    # parse config
     parser = argparse.ArgumentParser()
     parser.add_argument("--config", default="test",
-                        type=str, help="config for running booster.")
+                        type=str, help="config for running Unet.")
     args = parser.parse_args()
     config = args.config  # name of config
     print(f"\n ----> You chose config : {config} <---- \n")
     # load config
     configs = yaml.load(open("configs.yaml"))
-    opts = configs[config]  # dict of all options
-    params = opts["params"]  # dict of parameters for training booster
+    opts = configs[config]  #  dict of all options
+    params = opts["params"]  #  dict of parameters for training booster
     # load data
     X, y = load_data(opts["type_data"])
-    # cross validation 
-    booster = Xgboost(X, y, config, params)
-    booster.cross_validation(nrounds=opts["nrounds"], nfolds=opts["nfolds"], early_stop_rounds=opts["early_stop_rounds"])
+    # cross validation
+    booster = Catboost(X, y, config, params)
+    booster.cross_validation(
+        nrounds=opts["nrounds"], nfolds=opts["nfolds"], early_stop_rounds=opts["early_stop_rounds"])
     booster.print_results()
     booster.save_results()
     booster.save_preds()
     booster.save_models()
+
 
 if __name__ == '__main__':
     main()
