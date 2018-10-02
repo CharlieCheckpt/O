151aa45d0d08af7d07272b280dcce4c8173e968d
CharlieCheckpt <charlie.95@hotmail.fr>
2018-10-02 22:59:57 +0200

    debug

--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 import os
 import pickle
-DATA_PATH = '/home/checkpt/workspace/O/data'
+DATA_PATH = '/home/ubuntu/workspace/O/data'
 
 
 def load_data(type_data: str):
@@ -15,14 +15,14 @@ def load_data(type_data: str):
 
     if type_data == "18k":
         print("loading half data ...")
-        X = np.load(os.path.join(DATA_PATH, 'Xtrain_challenge_owkin_half.npy'))
-        y = np.load(os.path.join(DATA_PATH, 'Ytrain_challenge_owkin_half.npy'))
+        X = np.load(os.path.join(DATA_PATH, 'Xtrain_half_challenge_owkin.npy'))
+        y = np.load(os.path.join(DATA_PATH, 'Ytrain_challenge_owkin.npy'))
 
     elif type == "36k":
         print("loading full data ...")
         #  todo: create full matrix as .npy
         X = np.load(os.path.join(DATA_PATH, 'Xtrain_challenge_owkin.csv'))
-        y = np.load(os.path.join(DATA_PATH, 'Ytrain_challenge_owkin_half.npy'))
+        y = np.load(os.path.join(DATA_PATH, 'Ytrain_challenge_owkin.npy'))
     end = time.time()
     print(f"data loaded in {round(end-start, 3)} seconds")
 
