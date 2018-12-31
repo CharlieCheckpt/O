2df410c32a46ceeb78d695830372cbb967c6a065
Charlie Saillard <charlie.saillard@owkin.com>
2018-12-31 08:53:22 +0100

    better main()

--- catboost/run_cb.py
+++ catboost/run_cb.py
@@ -1,30 +1,21 @@
 import os
 import argparse
 import yaml
-
-import sys
-sys.path.insert(0, "../")
-from utils import load_data
+from O.utils import load_data
 from cb_model import Catboost
 
 
-def main():
-    # parse config
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--config", default="test",
-                        type=str, help="config for running Unet.")
-    args = parser.parse_args()
-    config = args.config  # name of config
+def main(name_config:str):
     print(f"\n ----> You chose config : {config} <---- \n")
     # load config
     configs = yaml.load(open("configs.yaml"))
-    opts = configs[config]  #  dict of all options
+    opts = configs[name_config]  #  dict of all options
     params = opts["params"]  #  dict of parameters for training booster
     name_data = opts["fn_X"].split(".")[0]  # name of data (without extension)
     # load data
     X, y = load_data(opts["fn_X"], opts["fn_y"])
     # cross validation
-    booster = Catboost(X, y, config, params, name_data)
+    booster = Catboost(X, y, name_config, params, name_data)
     booster.cross_validation(
         nrounds=opts["nrounds"], nfolds=opts["nfolds"], early_stop_rounds=opts["early_stop_rounds"])
     booster.print_results()
@@ -34,4 +25,9 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    # parse config
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config", default="test",
+                        type=str, help="config for running Unet.")
+    args = parser.parse_args()
+    main(args.config)
