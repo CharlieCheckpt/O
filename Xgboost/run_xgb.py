388e4aec45f1bbd1a66929f7369069068c0445dc
Charlie Saillard <charlie.saillard@owkin.com>
2018-12-31 08:38:44 +0100

    better main

--- Xgboost/run_xgb.py
+++ Xgboost/run_xgb.py
@@ -1,35 +1,33 @@
+"""Run xgboost, with specified parameters"""
 import os
-import argparse
+import click
 import yaml
-
-import sys
-sys.path.insert(0, "../")
-from utils import load_data
+from O.utils import load_data
 from xgb_model import Xgboost
 
 
-def main():
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--config", default="test",
-                        type=str, help="config for running booster.")
-    args = parser.parse_args()
-    config = args.config  # name of config
-    print(f"\n ----> You chose config : {config} <---- \n")
+def main(name_config:str):
+    print(f"\n ----> You chose config : {name_config} <---- \n")
     # load config
     configs = yaml.load(open("configs.yaml"))
-    opts = configs[config]  # dict of all options
+    opts = configs[name_config]  # dict of all options
     params = opts["params"]  # dict of parameters for training booster
     name_data = opts["fn_X"].split(".")[0]  #  name of data (without extension)
     # load data
     X, y = load_data(opts["fn_X"], opts["fn_y"])
     # cross validation 
-    booster = Xgboost(X, y, config, params, name_data)
+    booster = Xgboost(X, y, name_config, params, name_data)
     booster.cross_validation(nrounds=opts["nrounds"], nfolds=opts["nfolds"], early_stop_rounds=opts["early_stop_rounds"])
     booster.print_results()
     booster.save_results()
     booster.save_preds()
     booster.save_models()
 
+
 if __name__ == '__main__':
-    main()
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config", default="test",
+                        type=str, help="config for running booster.")
+    args = parser.parse_args()
+    main(args.config)
